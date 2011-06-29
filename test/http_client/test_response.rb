620f08e95c2ee0436aeb5dad099b8e67957b08e3
Adam Esterline <adam@esterlines.com>
2011-06-29 10:55:57 -0600

    Added response code to the response

--- test/http_client/test_response.rb
+++ test/http_client/test_response.rb
@@ -8,6 +8,12 @@ class ResponseTest < Test::Unit::TestCase
     assert_equal("baz", response.body)
   end
 
+  def test_response_code
+    get = HTTP::Get.new("/echo", :content => "baz")
+    response = @client.execute(get)
+    assert_equal(HTTP::Status::OK, response.status_code)
+  end
+
   def setup
     @client = HTTP::Client.new(:default_host => "http://localhost:8080")
   end
