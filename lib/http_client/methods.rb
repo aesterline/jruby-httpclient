100336f0d41f1e6f6c0194d3c188090241058ebd
Adam Esterline <adam@esterlines.com>
2011-06-28 20:54:57 -0600

    Cleaned up the HTTP method hierarchy

--- lib/http_client/methods.rb
+++ lib/http_client/methods.rb
@@ -2,86 +2,85 @@ require 'cgi'
 
 module HTTP
   class Request
-    def self.create_type(&native_request_factory)
-      Class.new do
-        attr_accessor :body, :encoding
+    attr_accessor :body, :encoding
 
-        def initialize(uri, params = {})
-          @uri = uri
-          @params = params
-          @encoding = "UTF-8"
+    def initialize(uri, params = {})
+      @scheme, @host, @port, @path, query = parse_uri(uri)
+      combined_params = CGI.parse(query || "").merge(params)
+      @query_params = combined_params.collect { |key, value| BasicNameValuePair.new(key.to_s, value.to_s) }
 
-          @headers = {}
-        end
+      @encoding = "UTF-8"
+      @headers = {}
+    end
 
-        def add_headers(headers)
-          @headers.merge!(headers)
-        end
+    def add_headers(headers)
+      @headers.merge!(headers)
+    end
 
-        def content_type=(type)
-          add_headers({'content-type' => type})
-        end
+    def content_type=(type)
+      add_headers({'content-type' => type})
+    end
 
-        def basic_auth(username, password)
-          @username = username
-          @password = password
-        end
+    def basic_auth(username, password)
+      @username = username
+      @password = password
+    end
 
-        def make_native_request(client, handler=nil)
-          request = create_native_request
-          request.entity = StringEntity.new(@body) unless @body.nil?
+    def make_native_request(client, handler=nil)
+      request = create_native_request
 
-          unless @username.nil?
-            client.credentials_provider.set_credentials(AuthScope::ANY, UsernamePasswordCredentials.new(@username, @password))
-          end
+      request.entity = StringEntity.new(@body) unless @body.nil?
+      @headers.each { |name, value| request.add_header(name.to_s, value.to_s) }
 
-          if handler
-            client.execute(request, handler)
-          else
-            client.execute(request)
-          end
-        end
-
-        def parse_uri
-          uri = URI.parse(@uri)
-          [uri.scheme, uri.host, uri.port, uri.path, uri.query]
-        rescue URI::InvalidURIError
-          [nil, nil, nil, @uri, nil]
-        end
-
-        private
-        define_method(:create_native_request) do
-          scheme, host, port, path, query = parse_uri
-          query_params = CGI.parse(query || "").merge(@params)
-
-          params = query_params.collect { |key, value| BasicNameValuePair.new(key.to_s, value.to_s) }
-          request = native_request_factory.call(scheme, host, port, path, params, @encoding)
-
-          @headers.each { |name, value| request.add_header(name.to_s, value.to_s) }
-
-          request
-        end
+      unless @username.nil?
+        client.credentials_provider.set_credentials(AuthScope::ANY, UsernamePasswordCredentials.new(@username, @password))
       end
+
+      if handler
+        client.execute(request, handler)
+      else
+        client.execute(request)
+      end
+    end
+
+    private
+    def create_uri(query_string = nil)
+      URIUtils.create_uri(@scheme, @host, @port, @path, query_string, nil)
+    end
+
+    def parse_uri(uri)
+      uri = URI.parse(uri)
+      [uri.scheme, uri.host, uri.port, uri.path, uri.query]
+    rescue URI::InvalidURIError
+      [nil, nil, nil, uri, nil]
     end
   end
 
-  Post = Request.create_type do |scheme, host, port, path, params, encoding|
-    post = HttpPost.new(URIUtils.create_uri(scheme, host, port, path, nil, nil))
-    post.entity = UrlEncodedFormEntity.new(params, encoding)
-    post
+  class Post < Request
+    def create_native_request
+      post = HttpPost.new(create_uri)
+      post.entity = UrlEncodedFormEntity.new(@query_params, encoding)
+      post
+    end
   end
 
-  Get = Request.create_type do |scheme, host, port, path, params, encoding|
-    query_string = URLEncodedUtils.format(params, encoding)
-    HttpGet.new(URIUtils.create_uri(scheme, host, port, path, query_string, nil))
+  class Get < Request
+    def create_native_request
+      query_string = URLEncodedUtils.format(@query_params, encoding)
+      HttpGet.new(create_uri(query_string))
+    end
   end
 
-  Delete = Request.create_type do |scheme, host, port, path|
-    HttpDelete.new(URIUtils.create_uri(scheme, host, port, path, nil, nil))
+  class Delete < Request
+    def create_native_request
+      HttpDelete.new(create_uri)
+    end
   end
 
-  Put = Request.create_type do |scheme, host, port, path|
-    HttpPut.new(URIUtils.create_uri(scheme, host, port, path, nil, nil))
+  class Put < Request
+    def create_native_request
+      HttpPut.new(create_uri)
+    end
   end
 
   private
